7e5ce485e4dcebe5a30617a412871e8f1cb1e9d3
Arnold Chand <creativenull@outlook.com>
2024-10-10 22:23:54 -0400

    feat: init transitions on home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,51 +1,59 @@
 <script setup lang="ts">
-definePageMeta({ layout: "dashboard", });
+definePageMeta({ layout: "dashboard" });
 useHead({ title: "CreativeNull / Home" });
+
+const loaded = useState('loaded', () => false)
+onMounted(() => (loaded.value = true));
 </script>
 
 <template>
   <AppTopBar />
 
   <main
-    class="text-defaults px-2 sm:px-0 pt-[3.5rem] font-departure min-h-[100vh] container mx-auto grid grid-cols-1 sm:grid-cols-2 gap-2"
+    class="text-defaults px-2 sm:px-0 pt-[4rem] font-departure min-h-[100vh] container mx-auto grid grid-cols-1 sm:grid-cols-2 gap-2"
   >
     <div class="space-y-4">
       <HomeCurrentTime />
       <HomeWeatherReport />
       <HomeFlowTimer />
+      <HomeTasks />
     </div>
     <div class="space-y-4">
-      <div class="space-y-4">
-        <h2 class="text-xl sm:text-3xl">Recent Posts</h2>
-        <div class="font-roboto space-y-2">
-          <div>
-            <div class="flex items-center gap-2">
-              <a href="#" class="underline">
-                Amet voluptatem rerum molestias harum
-              </a>
-              <span class="text-sm">2024-01-01</span>
-            </div>
-            <p class="text-sm">
-              Adipisicing et quisquam excepturi repellat iusto Voluptatibus
-              veritatis dolorem nam dolorum placeat cumque? Voluptate non
-              quibusdam molestias laudantium ab laudantium?
-            </p>
-          </div>
-          <div>
-            <div class="flex items-center gap-2">
-              <a href="#" class="underline"> Amet ipsum omnis quos adipisci </a>
-              <span class="text-sm">2024-01-01</span>
-            </div>
-            <p class="text-sm">
-              Adipisicing et quisquam excepturi repellat iusto Voluptatibus
-              veritatis dolorem nam dolorum placeat cumque? Voluptate non
-              quibusdam molestias laudantium ab laudantium?
-            </p>
-          </div>
-        </div>
-      </div>
+      <Transition name="fade-slide">
+        <HomeAboutMe v-if="loaded" />
+      </Transition>
+      <Transition name="fade-slide-delay">
+        <HomeRecentPosts v-if="loaded" />
+      </Transition>
     </div>
   </main>
 
   <HomeFooter />
 </template>
+
+<style scoped>
+.fade-slide-enter-active,
+.fade-slide-leave-active {
+  transition: all 0.5s ease;
+}
+.fade-slide-delay-enter-active,
+.fade-slide-delay-leave-active {
+  transition: all 0.5s ease 0.1s;
+}
+
+.fade-slide-enter-from,
+.fade-slide-leave-to,
+.fade-slide-delay-enter-from,
+.fade-slide-delay-leave-to {
+  opacity: 0;
+  transform: translateY(5px);
+}
+
+.fade-slide-enter-to,
+.fade-slide-leave-from,
+.fade-slide-delay-enter-to,
+.fade-slide-delay-leave-from {
+  opacity: 1;
+  transform: translateY(0);
+}
+</style>
