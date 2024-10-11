0190e32468c7e978d00ab5d0c8de80309b5e1a7e
Arnold Chand <creativenull@outlook.com>
2024-10-10 22:22:21 -0400

    fix: text and bg defaults on layout

--- components/AppTopBar.vue
+++ components/AppTopBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="fixed z-10 h-[3rem] w-full backdrop-blur bg-white/30 dark:bg-gray-800/30 text-defaults m-0 p-0">
+  <div class="fixed z-10 h-[3rem] w-full backdrop-blur bg-white/30 dark:bg-gray-800/30 m-0 p-0">
     <div class="container mx-auto px-4 sm:px-0 flex items-center justify-between h-full">
       <AppLogo />
       <TopBarMenu />
