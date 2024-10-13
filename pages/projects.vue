2f77a2975c788ddafdd6f3dfcd352c87ed01f9cb
Arnold Chand <creativenull@outlook.com>
2024-10-13 13:50:52 -0400

    feat: projects

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,3 +1,26 @@
 <template>
-  <h1>Coming Soon..</h1>
+  <div class="space-y-4">
+    <h2 class="text-xl sm:text-3xl">My open-source projects</h2>
+    <div class="font-roboto space-y-4">
+      <ContentList path="/projects" v-slot="{ list }">
+        <div v-for="project in list" :key="project.url" class="space-y-1">
+          <div class="text-lg">
+            <h2 class="inline-block">
+              <NuxtLink
+                :to="project.url"
+                class="underline rounded hover:(text-sky-600 underline) transition duration-200 focus:(outline-none ring-1 ring-gray-800) dark:focus:(ring-white)"
+                target="_blank"
+                tabindex="0"
+              >
+                {{ project.title }}
+              </NuxtLink>
+            </h2>
+          </div>
+          <p class="text-sm">
+            {{ project.description }}
+          </p>
+        </div>
+      </ContentList>
+    </div>
+  </div>
 </template>
