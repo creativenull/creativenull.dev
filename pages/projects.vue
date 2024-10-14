ba3669806a70f1ebab378935042ab0910a0d7f95
Arnold Chand <creativenull@outlook.com>
2024-10-14 10:31:50 -0400

    fix: lighter text

--- pages/projects.vue
+++ pages/projects.vue
@@ -16,7 +16,7 @@
               </NuxtLink>
             </h2>
           </div>
-          <p class="text-sm">
+          <p class="text-sm text-gray-600 dark:text-gray-400">
             {{ project.description }}
           </p>
         </div>
