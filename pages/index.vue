49a654bd67d03f7c9243fa18a64fa84381acee90
Arnold Chand <creativenull@outlook.com>
2024-10-18 21:37:34 -0400

    fix: revert to old view for mobile

--- pages/index.vue
+++ pages/index.vue
@@ -11,11 +11,13 @@ const { loaded } = useFadeSlideLoadingState();
     <main
       class="text-defaults px-2 sm:px-0 pt-[4rem] font-departure min-h-[100vh] container mx-auto grid grid-cols-1 sm:grid-cols-2 gap-2"
     >
-      <div class="sm:block hidden space-y-4">
+      <div class="space-y-4">
         <HomeCurrentTime />
         <HomeWeatherReport />
-        <HomeFlowTimer />
-        <HomeTasks />
+        <div class="sm:block hidden">
+          <HomeFlowTimer />
+          <HomeTasks />
+        </div>
       </div>
       <div class="space-y-4 mb-4">
         <Transition name="fade-slide">
