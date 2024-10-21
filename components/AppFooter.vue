8b7f5233869b6c84e4c0c9c549c1cba318e69872
Arnold Chand <creativenull@outlook.com>
2024-10-20 21:14:45 -0400

    feat: generic footer

---
+++ components/AppFooter.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="font-departure text-xs">
+    &copy; Arnold Chand (<NuxtLink
+      to="https://github.com/creativenull"
+      target="_blank"
+      tabindex="0"
+      class="underline rounded focus:(outline-none ring-1 ring-gray-800) dark:focus:(ring-white)"
+      >creativenull</NuxtLink
+    >) 2024
+  </div>
+</template>
