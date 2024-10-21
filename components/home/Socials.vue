f64481bc94a700c2bb37e12c8dbfa8babace596f
Arnold Chand <creativenull@outlook.com>
2024-10-20 21:01:05 -0400

    fix: underline for any links

--- components/home/Socials.vue
+++ components/home/Socials.vue
@@ -13,7 +13,7 @@ const appConfig = useAppConfig();
       <div v-for="social in appConfig.site.socials" :key="social.name">
         <NuxtLink
           :to="social.url"
-          class="flex items-center gap-1 no-underline rounded hover:(text-sky-600 underline) transition duration-200 focus:(outline-none ring-1 ring-gray-800) dark:focus:(ring-white)"
+          class="capitalize flex items-center gap-1 underline rounded hover:text-sky-600 transition duration-200 focus:(outline-none ring-1 ring-gray-800) dark:focus:(ring-white)"
           target="_blank"
           tabindex="0"
           :title="`navigate to ${social.name}`"
