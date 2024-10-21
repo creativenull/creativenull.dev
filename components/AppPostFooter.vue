ca481c147cf430194a6de577f5641ad1d97f2a70
Arnold Chand <creativenull@outlook.com>
2024-10-20 21:15:40 -0400

    feat: socials from appConfig, footer details

--- components/AppPostFooter.vue
+++ components/AppPostFooter.vue
@@ -1,45 +1,37 @@
+<script setup lang="ts">
+const appConfig = useAppConfig();
+</script>
+
 <template>
-  <footer
-    class="rounded-lg border border-gray-300 dark:border-gray-800 dark:text-gray-400 text-gray-600 max-w-[65ch] mx-auto p-4 my-10"
-  >
+  <footer class="border-t border-gray-800 dark:border-gray-200 pt-4 my-10">
     <div>
-      <h5 class="italic">
-        Enjoyed the post or have some constructive thoughts?
-      </h5>
+      <p class="italic">Enjoyed the post or have some constructive thoughts?</p>
       <p class="text-sm">Let me know what you think by messaging me on</p>
-      <ul class="text-sm space-y-2">
-        <li>
+      <nav class="text-sm space-y-2">
+        <div v-for="social in appConfig.site.socials" :key="social.name">
           <NuxtLink
-            to="https://x.com/creativenu11"
-            external
+            :to="social.url"
+            class="capitalize flex items-center gap-1 underline rounded hover:text-sky-600 transition duration-200 focus:(outline-none ring-1 ring-gray-800) dark:focus:(ring-white)"
             target="_blank"
             tabindex="0"
+            :title="`navigate to ${social.name}`"
           >
-            <div class="i-bi-twitter-x inline-block"></div>
-            @creativenu11
+            <div :class="`${social.iconClass} inline-block`"></div>
+            {{ social.name }}
           </NuxtLink>
-        </li>
-        <li>
-          <NuxtLink
-            to="https://mastodon.online/@creativenull"
-            external
-            target="_blank"
-            tabindex="0"
-          >
-            <div class="i-bi-mastodon inline-block"></div>
-            @creativenull@mastodon.online
-          </NuxtLink>
-        </li>
-      </ul>
+        </div>
+      </nav>
     </div>
 
     <div>
-      <h5 class="italic">About me</h5>
+      <p class="italic">About me</p>
       <p class="text-sm">
         I am a software developer and open-source enthusiast. I live in the
         island of Trinidad and Tobago as a freelance developer working mainly
         with PHP (Laravel), TypeScript, and Vuejs.
       </p>
     </div>
+
+    <AppFooter />
   </footer>
 </template>
