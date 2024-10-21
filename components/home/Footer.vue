077834270dffffc4454a093bad67451e84145421
Arnold Chand <creativenull@outlook.com>
2024-10-20 21:15:17 -0400

    fix: use AppFooter

--- components/home/Footer.vue
+++ components/home/Footer.vue
@@ -2,12 +2,6 @@
   <footer
     class="hidden sm:block fixed z-10 bottom-0 right-0 pr-4 pb-4 text-xs text-gray-700 dark:text-white font-departure"
   >
-    &copy; Arnold Chand (<NuxtLink
-      to="https://github.com/creativenull"
-      target="_blank"
-      tabindex="0"
-      class="underline rounded focus:(outline-none ring-1 ring-gray-800) dark:focus:(ring-white)"
-      >creativenull</NuxtLink
-    >) 2024
+    <AppFooter />
   </footer>
 </template>
