351ab55acd915d42dea72380d3ff3ad64211311e
Arnold Chand <creativenull@outlook.com>
2024-12-02 09:47:13 -0400

    feat: show tags in post

--- pages/posts/[...slug].vue
+++ pages/posts/[...slug].vue
@@ -11,6 +11,11 @@ const { loaded } = useFadeSlideLoadingState();
           <div v-if="loaded">
             <h1 class="text-2xl sm:text-4xl mb-0">{{ doc.title }}</h1>
             <span class="text-xs">{{ getLocaleDate(doc.publishDate) }}</span>
+            <div class="inline-block text-xs ml-1">
+              <span v-for="tag in getTagsFromString(doc.tags)" :key="tag">
+                #{{ tag }}
+              </span>
+            </div>
           </div>
         </Transition>
 
