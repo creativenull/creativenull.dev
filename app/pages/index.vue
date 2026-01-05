648de8668f8f61fa345cab6a732d38cfce5b5db2
Arnold Chand <creativenull@outlook.com>
2026-01-04 21:38:34 -0400

    fix: hide sidebar starting from tablet view

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -9,13 +9,14 @@ const { loaded } = useFadeSlideLoadingState();
 <template>
   <div>
     <main
-      class="text-defaults px-2 sm:px-0 pt-[4rem] min-h-[100vh] container mx-auto grid grid-cols-1 sm:grid-cols-2 gap-2"
+      class="text-defaults px-2 md:px-0 pt-[4rem] min-h-[100vh] container mx-auto grid grid-cols-1 md:grid-cols-2 gap-2"
     >
       <div class="font-departure space-y-4">
         <HomeCurrentTime />
         <HomeWeatherReport />
-        <div class="sm:block hidden">
+        <div class="md:block hidden">
           <HomeFlowTimer />
+          <HomeCalendar />
           <HomeTasks />
         </div>
       </div>
@@ -31,7 +32,7 @@ const { loaded } = useFadeSlideLoadingState();
         </Transition>
       </div>
 
-      <div class="block sm:hidden mb-8">
+      <div class="block md:hidden mb-8">
         <AppFooter />
       </div>
     </main>
