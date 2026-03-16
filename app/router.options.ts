c7d1395863500d0537d0d0e6e07888a92166ed2f
Arnold Chand <creativenull@outlook.com>
2026-03-15 21:51:50 -0400

    fix: handle scroll on hash and default routes

--- app/router.options.ts
+++ app/router.options.ts
@@ -1,17 +1,15 @@
 import type { RouterConfig } from "@nuxt/schema";
 
 export default {
-  scrollBehavior(to, from, savedPosition) {
-    return new Promise((resolve, _reject) => {
-      window.setTimeout(() => {
-        if (to.hash) {
-          resolve({
-            el: to.hash,
-            top: 65,
-            behavior: "smooth",
-          });
-        }
-      }, 150);
-    });
+  scrollBehavior(to, _from, _savedPosition): { el?: string; top: number; behavior: "smooth" } {
+    if (to.hash) {
+      return {
+        el: to.hash,
+        top: 65,
+        behavior: "smooth",
+      };
+    } else {
+      return { top: 0, behavior: "smooth" };
+    }
   },
 } satisfies RouterConfig;
