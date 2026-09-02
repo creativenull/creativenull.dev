1033907eb9f77d5e93f89c1b62fdd8ca4da9f06e
Arnold Chand <creativenull@outlook.com>
2026-09-01 22:01:00 -0400

    fix(AppPostFooter): update whoami content

--- app/components/AppPostFooter.vue
+++ app/components/AppPostFooter.vue
@@ -24,9 +24,8 @@ const socials = appConfig.site.socials.filter((s) => s.name !== "GitHub");
     <div class="my-4">
       <h6 class="text-sm font-jetbrains">whoami</h6>
       <p class="text-sm my-1">
-        A freelance software developer from Trinidad and Tobago 🇹🇹 who is passionate about software development and
-        open-source work and maybe a
-        <em>tad bit</em> obsessed with tweaking my vim configuration.
+        I am a self-employed software developer from Trinidad &amp; Tobago 🇹🇹. working with Laravel and Vue
+        applications. I am passionate about open-source and maybe a bit obsessed with configuring vim.
       </p>
     </div>
 
